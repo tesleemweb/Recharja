fef06e7192e22c72a8c71e31b32887ed6da50574
tesleemweb <tesleem393@gmail.com>
2025-11-27 14:58:36 +0100

    Change email transport port to 587

--- To GitHub/backend/utils/email.js
+++ To GitHub/backend/utils/email.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
-  port: 465,
+  port: 587,
   secure: true,
   auth: {
     user: process.env.MAIL_USER,
@@ -29,3 +29,4 @@ const sendEmail = async ({ to, subject, html }) => {
 };
 
 module.exports = sendEmail;
+
